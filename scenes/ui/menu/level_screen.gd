92252253bf1e8f48d862f0b127c8e8f5a57b96eb
dalifyyy <d4lified@gmail.com>
2026-03-21 17:58:24 +0800

    .

--- scenes/ui/menu/level_screen.gd
+++ scenes/ui/menu/level_screen.gd
@@ -1,11 +1,25 @@
 extends Control
 
+@onready var levels = $levels
+@onready var lines = $level_lines
 
-# Called when the node enters the scene tree for the first time.
+var level_stars := []
+var level_lines := []
+
 func _ready():
-	pass # Replace with function body.
+	for entry in levels.get_children():
+		if entry is LevelStar:
+			level_stars.append(entry)
+	
+	for i in len(level_stars) - 1:
+		var line = Line2D.new()
+		line.width = 4.0
+		line.default_color = Color("7c8497")
+		
+		level_lines.append(line)
+		lines.add_child(line)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	for i in range(level_lines.size()):
+		level_lines[i].points = [level_stars[i].position, level_stars[i + 1].position]
+		
--- scripts/components/level_star.gd
+++ scripts/components/level_star.gd
@@ -1,11 +1,15 @@
+@tool
 class_name LevelStar extends Sprite2D
-
 @onready var sprite := preload("res://assets/sprites/icons/level_star.png")
 
 func _ready():
+	texture = sprite
+	var r = randi_range(15, 15)
+	var t = randf_range(0.6, 1.0)
+	
 	var tween = create_tween()
 	tween.set_loops()
 	tween.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	
-	tween.tween_property($Sprite2D, "rotation_degrees", 15.0, 0.6)
-	tween.tween_property($Sprite2D, "rotation_degrees", -15.0, 0.6)
+	tween.tween_property(self, "rotation_degrees", 40.0 + r, t)
+	tween.tween_property(self, "rotation_degrees", -40.0 - r, t)
