871d54288837347d98ac781cee39a6903486c9de
dalifyyy <d4lified@gmail.com>
2026-03-22 09:59:32 +0800

    .

--- scenes/ui/menu/level_screen.gd
+++ scenes/ui/menu/level_screen.gd
@@ -8,6 +8,7 @@ var level_stars := []
 var level_lines := []
 
 var current_level := 0
+var switching_level := false
 var max_level
 
 func _ready():
@@ -31,17 +32,21 @@ func _ready():
 func _process(_delta):
 	for i in range(level_lines.size()):
 		level_lines[i].points = [level_stars[i].position, level_stars[i + 1].position]
-		
+	
+	if switching_level: return
+	
 	if Input.is_action_just_pressed("move_left"):
-		current_level = max(0, current_level - 1)
-		
-		pointer.position = level_stars[current_level].position
-		pointer.position.y -= 18
-		pointer.position.x -= 5.5
-		
+		change_level_pointer(-1)
 	if Input.is_action_just_pressed("move_right"):
-		current_level = min(max_level - 1, current_level + 1)
-		
-		pointer.position = level_stars[current_level].position
-		pointer.position.y -= 18
-		pointer.position.x -= 5.5
+		change_level_pointer(1)
+
+func change_level_pointer(offset):
+	switching_level = true
+	current_level = max(0, min(max_level - 1, current_level + offset))
+	
+	var tw = get_tree().create_tween()
+	tw.tween_property($pointer, "position", Vector2(level_stars[current_level].position.x - 5.5, level_stars[current_level].position.y - 18), 0.24)
+	tw.tween_callback(func(): switching_level = false)
+	
+	
+	
